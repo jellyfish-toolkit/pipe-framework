ba478e54e78d55d5b4c84f84f69a78f9e9568c2e
elias-prodigy <ilyazp90@gmail.com>
2020-03-24 13:33:27 +0200

    changes in LDBLoader
    several improvements
    
    LDBLoader is ready
    
    Loader to database is ready

--- keeneye/db/load.py
+++ keeneye/db/load.py
@@ -10,50 +10,32 @@ from sqlalchemy.engine.url import URL
 class LDBLoaderBase(Loader):
     connection_string: t.Optional[str] = None
     connection_conf: dict = {}
-    meta = MetaData(engine)
-    some_table = Table(
-        'user', meta,
-        Column('data', String),
-    )
-    meta.create_all(engine)
 
-    def __init__(self, connection_string, connection_conf, store_class=Store):
-        self.connection_string = connection_string
-        self.connection_conf = connection_conf
-        self.__store_class = store_class
+    def __init__(self, data_field: t.Optional[str] = None, table_name: t.Optional[str] = None):
+        self.table_name = table_name
+        self.data_field = data_field
 
-    def sqlalchemy_conn_db(self):
-        if connection_string is not None:
-            url = URL(**connection_string)
+    def get_db_conn(self):
+        if self.connection_string:
+            url = self.connection_string
         else:
-            url = URL(**connection_conf)
+            url = URL(**self.connection_conf)
         engine = create_engine(url, echo=True)
         connection = engine.connect()
         return connection
 
-    def insert_data(self, connection, some_table):
-        insert_statement = some_table.insert().values(self.store_class)
-        connection.execute(insert_statement)
+    def get_data(self, store: Store):
+        data = store.get(self.data_field)
+        return data
 
-    def update_data(self, connection, some_table):
-        update_statement = some_table.update().where(some_table.c.data).values(self.store_class)
-        connection.execute(update_statement)
+    def insert_data(self, data):
+        insert_statement = self.table_name.insert().values(data)
+        self.get_db_conn().execute(insert_statement)
 
+    def update_data(self, data):
+        update_statement = self.table_name.update().values(data)
+        self.get_db_conn().execute(update_statement)
 
-class LDBLoaderPostgres(LDBLoaderBase):
-    connection_string: str = 'postgresql://username:qwerty1@127.0.0.1:8000/mydb'
-    # connection_conf = dict(
-    #     drivername='postgresql',
-    #     username='username',
-    #     password='qwerty1',
-    #     host='127.0.0.1',
-    #     port='8000',
-    #     database='mydb',
-    # )
-
-# class LUserWithReallyHardToUnderstandRelations(LDBLoaderPostgres):
-#
-#   def load(self, store: Store):
 
 
 
