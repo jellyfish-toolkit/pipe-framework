187ccdc5aed63524b8c7272d3bb36fab60a1165b
elias-prodigy <ilyazp90@gmail.com>
2020-03-27 13:00:10 +0200

    working on the load method
    load method in progress
    
    load method in progress 2

--- pipe-framework/pipe/generics/db/load.py
+++ pipe-framework/pipe/generics/db/load.py
@@ -1,6 +1,6 @@
 import typing as t
 
-from sqlalchemy import create_engine, MetaData, Table, Column, String
+from sqlalchemy import create_engine
 
 from pipe.core.data import Store
 from pipe.core.base import Loader
@@ -11,9 +11,14 @@ class LDBLoaderBase(Loader):
     connection_string: t.Optional[str] = None
     connection_conf: dict = {}
 
-    def __init__(self, data_field: t.Optional[str] = None, table_name: t.Optional[str] = None):
+    def __init__(self,
+                 data_field: t.Optional[str] = None,
+                 table_name: t.Optional[str] = None,
+                 primary_key: t.Optional[str] = None
+                 ):
         self.table_name = table_name
         self.data_field = data_field
+        self.primary_key = primary_key
 
     def get_db_conn(self):
         if self.connection_string:
@@ -35,6 +40,18 @@ class LDBLoaderBase(Loader):
     def update_data(self, data):
         update_statement = self.table_name.update().values(data)
         self.get_db_conn().execute(update_statement)
+
+    def load(self):
+        if self.primary_key in self.data:
+            self.update_data(self.data)
+        elif self.primary_key not in self.data:
+            self.insert_data(self.data)
+        else:
+            raise Exception("Can't insert/update the data in the DB")
+
+        # if self.connection.has_table(self.table_name) is None:
+        #     raise Exception("There is no such table in the DB")
+
 
 
 
