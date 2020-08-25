453335301c483b0e51ccb550022981b5a0b8edab
roman <rlatyshenko.dev@gmail.com>
2020-08-25 18:43:22 +0300

    Docs for github-pages
    Signed-off-by: roman <rlatyshenko.dev@gmail.com>

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../../pipe-framework'))
+sys.path.insert(0, os.path.abspath('../pipe-framework'))
 from recommonmark.transform import AutoStructify
 
 source_suffix = {
@@ -14,9 +14,9 @@ master_doc = 'index'
 
 github_doc_root = 'https://github.com/jellyocean/pipe-framework/tree/master/docs/api/'
 
-html_theme_options = {
-    'nosidebar': True
-}
+# html_theme_options = {
+#     'nosidebar': True
+# }
 
 def setup(app):
     app.add_config_value('recommonmark_config', {
---
+++ docs/_sources/index.md.txt
@@ -0,0 +1,29 @@
+```eval_rst
+.. _contents:
+```
+
+`pipe-framework` documentation contents
+=============================
+
+```eval_rst
+.. toctree::
+   :maxdepth: 2
+   :glob:
+
+   api/*
+```
+
+Indices and tables
+==================
+
+```eval_rst
+.. only:: builder_html
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
+
+.. only:: not builder_html
+
+   * :ref:`modindex`
+```
