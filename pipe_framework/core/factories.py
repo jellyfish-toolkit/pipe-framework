5d52421098b93d283bfd0f371fe1e00ef509ec6c
Roman Latyshenko <rlatyshenko@gmail.com>
2022-05-02 00:43:03 +0300

    exile

--- pipe_framework/core/factories.py
+++ pipe_framework/core/factories.py
@@ -16,18 +16,6 @@ def create_hooks(*, before=None, after=None, interrupt=None):
 T = TypeVar("T")
 
 
-# class create_simple_pipe(Generic[T]):
-#     def __new__(
-#         cls, callables: Tuple[Step[T], ...], state: T, hooks: Hooks | None = None
-#     ):
-#         if hooks is None:
-#             hooks = create_hooks()
-
-#         _pipe = SimplePipe(callables, state, hooks)
-
-#         return _pipe
-
-
 def create_simple_pipe(
     callables: Tuple[Step[T], ...], state: T, hooks: Hooks | None = None
 ) -> SimplePipe[T]:
